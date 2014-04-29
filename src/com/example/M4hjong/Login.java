5044a8ae9fb7341cbbd25bd0b9d42b9bae133b69
Heather <heather.dykstra@colorado.edu>
2014-04-28 21:27:35 -0600

    Final commits

--- src/com/example/M4hjong/Login.java
+++ src/com/example/M4hjong/Login.java
@@ -1,38 +1,61 @@
 package com.example.M4hjong;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
-/**
- * Created by Michael on 3/1/14.
- */
 public class Login extends Activity {
-    //add more buttons... refactor needed
-    Button button1, button2, button3;
-    public void onCreate(Bundle savedInstanceState) {
+
+    private EditText username = null;
+    private EditText password = null;
+    private TextView attempts;
+    private Button login, button2, button3;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        username = (EditText) findViewById(R.id.editText1);
+        password = (EditText) findViewById(R.id.editText2);
+        attempts = (TextView) findViewById(R.id.textView5);
+        attempts.setText(Integer.toString(counter));
+        login = (Button) findViewById(R.id.button1);
         addListenerOnButton();
     }
 
-    public void addListenerOnButton() {
-
-        button1 = (Button) findViewById(R.id.logInFinalButton);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginFinalIntent =
-                        new Intent(Login.this, LoggedInMain.class);
-                        //new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-                startActivity(loginFinalIntent);
+    public void login(View view) {
+        if (username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+            newOnClick(view);
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if (counter == 0) {
+                login.setEnabled(false);
             }
 
-        });
+        }
+
+    }
+    public void newOnClick(View view) {
+        Intent loginFinalIntent =
+                new Intent(Login.this, LoggedInMain.class);
+        startActivity(loginFinalIntent);
+    }
+
+    public void addListenerOnButton() {
 
         button2 = (Button) findViewById(R.id.forgotButton);
 
@@ -44,7 +67,7 @@ public class Login extends Activity {
                 startActivity(forgotIntent);
             }
         });
-        button3 = (Button) findViewById(R.id.newButton);
+        button3 = (Button) findViewById(R.id.button3);
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
